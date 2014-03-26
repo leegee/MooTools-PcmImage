d22095b9d8981f5a3e66d9a150de29b13b41de4a
Lee Goddard <lee@leegoddard.net>
2014-03-26 15:21:50 +0100

    Stabbing at frequency colouring. Replays. Failed real-time freq.

--- Source/PcmImage.js
+++ Source/PcmImage.js
@@ -17,9 +17,9 @@ provides: [PcmImage]
 */
 
 /*
-	Version 0.4
+	Version 0.5
 
-	This code is copyright (C) 2012 Lee Goddard, Server-side Systems Ltd.
+	This code is copyright (C) 2012 Lee Goddard.
 	All Rights Reserved.
 	
 	Available under the same terms as Perl5.
@@ -38,16 +38,18 @@ var PcmImage = new Class({
 	Implements: [Options, Events],
 	
 	options: {
-		element: 	null,	/* conatiner to replace with canvas/image */
-		uri: 		null,	/* uri of sound */
-		strokestyle: null,	/* foreground colour, may come from css */
-		background:  null,	/* background colour, may come from css */
-		linewidth: 	1,		/* width of line used in graph */
-		step:		4,		/* process PCM in steps */
-		asimg:		false,	/* replace canvas with image, prevents playable */
-		playable:	true,	/* can the image be clicked to play? */
-		overlayclr:	'red',	/* overlaid when image played */
-		fftSize: 	512,
+		element: 		null,	/* conatiner to replace with canvas/image */
+		uri: 			null,	/* uri of sound */
+		strokestyle: 	null,	/* foreground colour, may come from css if possible */
+		background:  	null,	/* background colour, may come from css if possible */
+		linewidth: 		1,		/* width of line used in graph */
+		step:			4,		/* Graph PCM in steps */
+		asimg:			false,	/* replace canvas with image, prevents playable */
+		playable:		true,	/* can the image be clicked to play? */
+		overlayclr:		'red',	/* overlaid when image played */
+		updateInterval: 60/40, 	/* Graph overlay update frequency in milliseconds */
+		fftSize: 		1024,	/* FFT bin size */
+		onSoundLoaded: function(){},
 		onXhrError: function(){ throw 'XHR Error getting '+this.options.uri },
 		onNoBufferError: function(){
 			throw 'Error decoding file data from '+self.options.uri;
@@ -58,17 +60,20 @@ var PcmImage = new Class({
 	buffer: 		null,	/* Audio buffer object */
 	canvas:			null,	/* Canvas element added to options.element */
 	actx:			null,	/* Audio context object */
+	octx: 			null, 	/* Offline audio context object */
     cctx:			null,	/* Canvas context object */
     img:			null,	/* May hold an img element */
     audioReady:		false,	/* True when sound loaded */
     playing:		false,	/* True when audio is playing */
-    pauseTimer:		null,	/* Used to togglePlay at end of sound */
     renderTimer:	null,	/* Rendering the overlay during play */
-    nowSeconds:		0,		/* Current time ini sound, for pausing */
+    pauseTimer: 	null,	/* Stops the renderTimer */
+    playbackTime:		0,		/* Current time ini sound, for pausing */
     width:			0,		/* Size of visual element */
     height:			0,		/* Size of visual element */
-    
-	initialize: function( options, actx ){
+    overlay: 		{},
+    freq_rgb: 		[],
+
+	initialize: function( options ){
 		var self = this;
 		this.setOptions(options);
 		
@@ -76,82 +81,81 @@ var PcmImage = new Class({
 			document.id(this.options.element) 
 			: this.element = this.options.element;
 
-		if (!this.element) console.error(options);
-			
+		if (!this.element) throw 'No valid options.element';
+
+		if (typeof webkitAudioContext == "function"){
+			window.AudioContext = webkitAudioContext;
+			window.OfflineAudioContext = webkitOfflineAudioContext;
+		}
+		if (typeof AudioContext != 'function') throw new Error('This browser does not support Web Audio Context');
+
 		if (typeof this.options.playable == "string"
 		  && this.options.playable.match(/^(0|false|)$/)) this.options.playable = false;
 
 		this.options.background = this.options.background 
-			|| this.element.getStyle('backgroundColor');	
+			|| this.element.getStyle('backgroundColor') || 'transparent';
 		this.options.strokestyle = this.options.strokestyle || this.element.getStyle('color');	
 
-		if (actx)  
-			this.actx = actx;
-		else if (typeof AudioContext == "function") 
-			this.actx = new AudioContext();
-		else if (typeof webkitAudioContext == "function") 
-			this.actx = new webkitAudioContext();
-		else throw('This browser does not support Web Audio Context');	
+		this.setClrs();
 
 		if (this.options.playable){
 			// Convert colors to standard format to allow names and shorthand hex:
 			var c = new  Element('div',{styles:{color:this.options.overlayclr}}).getStyle('color')
-			this.overlay = {};
 			this.overlay.fg   = {};
 			this.overlay.fg.r = parseInt( '0x'+c.substr(1,2) );
 			this.overlay.fg.g = parseInt( '0x'+c.substr(3,2) );
 			this.overlay.fg.b = parseInt( '0x'+c.substr(5,2) );
 		}
 		
-		if (this.options.asimg 
-		 && this.options.asimg != 'false'
-	 	 && this.options.asimg != '0'
-	 	){
-	 		this.options.asimg = true
+		if (this.options.asimg && this.options.asimg.match(/^(0|false|)$/)) {
+	 		this.options.asimg = true;
 	 	}
 		
-		this.initCanvas();
+		this.initGraphics();
 		this.fireEvent('canvasLoaded');
 		this.load();
 	},
 
-	initCanvas: function(){
+	initGraphics: function(){
 		this.width  = this.options.width  || this.element.getComputedSize().totalWidth;
 		this.height = this.options.height || this.element.getComputedSize().totalHeight;
 		var attr = {
 			width: this.width,
 			height: this.height,
 			styles: {
+				position: 'relative',
+				display: 'block',
+				zIndex: 2,
 				left: 0,
 				top: 0,
-				backgroundColor: this.options.background || 'transparent',
-				position: 'relative',
-				zIndex: 2,
-				border: 0,
-				margin: 0,
-				display: 'block',
 				width: this.width,
-				height: this.height
-			}
+				height: this.height,
+				backgroundColor: this.options.background
+			},
+			'class': this.options.element.get('class')
 		};
 		
 		this.canvas = new Element('canvas',attr);
 		if (this.options.asimg) this.img = new Element('img',attr);
-		
-		this.options.element.appendChild( this.canvas );
+	
+		this.canvas.replaces( this.options.element );
+		this.element = this.canvas;
 		this.cctx = this.canvas.getContext('2d');
 	},
 		
 	load: function(){
-		var self=this;
-		var request=new XMLHttpRequest();
+		var self = this;
+		var request = new XMLHttpRequest();
 		request.open("GET", this.options.uri, true);
-		request.responseType="arraybuffer";
-		request.onload=function(){
+		request.responseType = "arraybuffer";
+		request.onload = function loaded(){
+			self.actx = new AudioContext();
 			self.actx.decodeAudioData(request.response,function(buffer){
 				if (!buffer){
 					alert('error decoding file data: '+self.options.uri);
-				} else {
+					throw 'File decoding error';
+				} 
+				else {
 					self.buffer = buffer;
 					self.audioReady = true;
 					self.fireEvent('soundLoaded');
@@ -192,12 +196,13 @@ var PcmImage = new Class({
 		var self = this;
 		var cd = [];
 		
+		this.offlineNode();
+
 		this.cctx.beginPath();
 		this.cctx.strokeStyle = this.options.strokestyle;
-		this.cctx.lineWidth = this.options.linewidth;
+		this.cctx.lineWidth   = this.options.linewidth;
 		
-		// XXX TODO UGLY HACK!
-		this.cctx.moveTo( 0, this.height/1.9);
+		this.cctx.moveTo( 0, this.height/2);
 
 		for (var c=0; c < this.buffer.numberOfChannels; c++)
 			cd[c] = this.buffer.getChannelData( c );
@@ -219,25 +224,22 @@ var PcmImage = new Class({
 		
 		this.cctx.stroke();
 		
-		if (this.options.asimg 
-		 && this.options.asimg != 'false'
-	 	 && this.options.asimg != '0'
-	 	){
+		if (this.options.asimg ){
 			// store the current globalCompositeOperation
 			var compositeOperation = this.cctx.globalCompositeOperation;
 			
-			// set to draw behind current content
+			// Set to draw behind current content
 			this.cctx.globalCompositeOperation = "destination-over";
 			
-			// set background color
 			this.cctx.fillStyle = this.options.background;
-			
-			// draw background / rect on entire canvas
 			this.cctx.fillRect(0,0, this.canvas.width, this.canvas.height);
 
 	 		this.img.src = this.canvas.toDataURL();
 			this.img.replaces( this.canvas );
 			
+			// Restore the previous state
+			this.cctx.globalCompositeOperation = compositeOperation;
+
 			if (this.options.playable){
 				this.img.addEvent('click', function(){
 					self.togglePlay();	
@@ -257,21 +259,34 @@ var PcmImage = new Class({
 		else this.play();
 	},
 	
-	_stop: function( pause ) {
-		if (!this.playing) return;
-		this.playing = false;
-		this.node.stop( 0 );
-		this.nowSeconds = pause? this.actx.currentTime : 0;
-		clearTimeout( this.pauseTimer );
-		clearTimeout( this.renderTimer );
-	},
-	
 	pause: function(){
 		this._stop(true);
 	},
 	
 	stop: function(){
 		this._stop(false);
+	},
+
+	_stop: function( pause ) {
+		// console.log('_stop', this.playing, 'pause?', pause);
+		if (!this.playing) return;
+		this.playbackTime = pause? this.now() : 0;
+		this.node.stop();
+		clearInterval( this.renderTimer );
+		clearTimeout( this.pauseTimer );
+		this.playing = false;
+		if (! pause){
+			this.playbackTime = 0;
+		}
+		console.log( (pause? 'paused':'stopped'), ' at ', this.playbackTime )
+	},
+	
+	/** Specs say that the audio context currentTime is the time the audio context was created,
+	    always moves forwards, and cannot be stopped or paused. now() is relative to the buffer.
+	    {@see this#_actxStartTime}
+	*/
+	now: function(){
+		return this.playbackTime + this.actx.currentTime - this._actxStartTime;
 	},
 
 	play: function(){
@@ -281,81 +296,191 @@ var PcmImage = new Class({
 
 		this.setNode();
 
-		// void start(optional double when = 0, optional double offset = 0, optional double duration);
-		this.node.start( 
-			0, 
-			this.nowSeconds
-		);
+		// Reset if done:
+		if (this.playbackTime > this.node.buffer.duration){
+			this.playbackTime = 0;
+			this.render()
+		}
 
-		this.pauseTimer = this.stop.delay(
-			this.buffer.duration * 1000,
-			this
-		);
-		
-		this.overlayInterval = 50; // quater second update
-		var overlaySteps = ((this.buffer.duration*1000) / this.overlayInterval );
-		this.overlay.inc = this.width / overlaySteps;
-		this.overlay.lastX = this.overlay.inc * -1;
-		this.overlay.thisX = 0;
-		
-		// Why? What's wrong with my maths?
-		this.overlay.inc /= 1.666666666666; // nearly
-		
-		/*
-		console.log( 'width '+this.width );
-		console.log( 'duration '+(this.buffer.duration * 1000));
-		console.log( this.overlay.inc +' * '+ this.overlayInterval +' = ');
-		console.log( this.overlayInterval * this.overlay.inc );
-		*/
-		
+		// Render callback
 		this.renderTimer = this.overlayImg.periodical( 
-			this.overlayInterval,
+			this.options.updateInterval,
 			this 
 		);
-	},
-	
-	overlayImg: function(){
-	//	this.cctx.fillStyle = 'rgba( 0, 255, 255, 1)';
-		this.overlay.lastX = this.overlay.thisX;
-		this.overlay.thisX += this.overlay.inc;
-		
-		var imgd = this.cctx.getImageData( 
-			this.overlay.lastX, 0,
-			this.overlay.thisX, this.canvas.height
+
+	//	this.pauseTimer = this._stop(false).delay(
+	//		1000* (this.playbackTime + this.node.buffer.duration )
+	//	);
+
+		this.node.start( 
+			0, 
+			this.playbackTime	// 0 || where last paused
 		);
+
+		this.fromX = this.playbackTime * this.overlay.pxPerSec;
+
+		// '.pause' needs a place to start
+		this._actxStartTime = this.actx.currentTime;
+
+		console.log( 'started at ', this.playbackTime, "("+this._actxStartTime+")" );
+	},
+
+
+	overlayImg: function(){
+		this.overlay.lastX = 
+			(typeof this.overlay.thisX == 'undefined')?
+			0 : this.overlay.thisX-1;
 		
-		var found=0;
-		for (var i=0; i < imgd.data.length; i+=4){
-			// var update = 0;
-			// for (var p=0; p<3; p++){
-			// 	if (imgd.data[i+p] > 0) update ++;
-			// }
-			// if (update==3) {
+		this.overlay.thisX = this.now() * this.overlay.pxPerSec;
+
+		// console.info( this.now() +': ', this.overlay.lastX,'->', this.overlay.thisX);
+
+		// Don't allow too-frequent calls:
+		if (this.overlay.thisX - this.overlay.lastX <= 1) return;
+
+		// if (this.overlay.thisX > this.element.width){
+		if (this.now() >= this.node.buffer.duration){
+			this.stop();
+			return;
+		}
+
+		// this.cctx.globalAlpha = .5;
+		// this.cctx.globalCompositeOperation = 'xor';
+
+		// If we error, cancel playback/rendering.
+		try {
+			var imgd = this.cctx.getImageData( 
+				this.overlay.lastX, 0,
+				(this.overlay.thisX - this.overlay.lastX), this.canvas.height
+			);
+			
+			for (var i=0; i <= imgd.data.length; i+=4){
 				imgd.data[i]	= this.overlay.fg.r;
 				imgd.data[i+1]	= this.overlay.fg.g;
 				imgd.data[i+2]	= this.overlay.fg.b;
-			// }
-		}
+				// imgd.data[i+3]  = 255; // imgd.data[i+3];
+			}
 
-		this.cctx.putImageData(imgd, this.overlay.lastX, 0);
+			this.cctx.putImageData(imgd, this.overlay.lastX, 0);
+		}
+		catch (e) {
+			this.stop();
+		}
+	},
+
+	/** Offline audio processing is faster than real time.
+	    Used here to apply frequency analysis to colour the wave.
+	    Sets a few parameters for use during playback. */
+	offline_overlayImg: function(e){
+		if (typeof this.overlay.pxPerSec == 'undefined'){
+			this.overlay.pxPerSec = this.width / this.buffer.duration;
+			//console.info( 'this.overlay.pxPerSec = ', this.overlay.pxPerSec)
+			//console.log( 'width = ', this.width, ', total px = ', this.overlay.pxPerSec * this.buffer.duration);
+		}
+		
+		var fromX =  e.playbackTime * this.overlay.pxPerSec;
+		var toX   =  fromX + ( e.inputBuffer.duration * this.overlay.pxPerSec);
+		
+		if (parseInt( toX ) > parseInt(fromX)){	
+		    var data =  new Uint8Array( this.offline_analyser.frequencyBinCount );
+		    this.offline_analyser.getByteFrequencyData(data);
+		    var values = 0;
+		    for (var j=0; j < data.length; j++) {
+		        values += data[j];
+		    }
+		    var average = parseInt( values / data.length );
+			// console.log(e.playbackTime, ': ', average, ' @ ', fromX, toX );
+
+			// this.cctx.globalAlpha = .5;
+			// this.cctx.globalCompositeOperation = 'xor';
+
+			var imgd = this.cctx.getImageData( 
+				fromX, 0,
+				toX, this.canvas.height
+			);
+			// console.log( this.freq_rgb[ average ], average)
+			for (var i=0; i < imgd.data.length; i+=4){
+				imgd.data[i]	= this.freq_rgb[ average ][0]; // this.overlay.fg.r;
+				imgd.data[i+1]	= this.freq_rgb[ average ][1]; // this.overlay.fg.g;
+				imgd.data[i+2]	= this.freq_rgb[ average ][2]; // this.overlay.fg.b;
+				// imgd.data[i+3]	= 1;
+			}
+
+			this.cctx.putImageData(imgd, fromX, 0);
+		}
+	},
+
+	offlineNode: function(){
+		var self = this;
+		if (this.buffer === null) throw 'setNode not caled, no buffer!';
+		
+		this.octx = new OfflineAudioContext( this.buffer.numberOfChannels, this.buffer.length, this.buffer.sampleRate );
+
+		this.offline_node = this.octx.createBufferSource();
+
+		this.offline_analyser = this.octx.createAnalyser();
+		this.offline_analyser.fftSize = this.options.fftSize;
+		this.offline_analyser.smoothingTimeConstant = .9;
+
+		this.offline_processor = this.octx.createScriptProcessor( this.options.fftSize, this.buffer.numberOfChannels, this.buffer.numberOfChannels);
+		this.offline_processor.connect( this.octx.destination );
+
+		this.offline_analyser.connect( this.offline_processor );
+
+		this.offline_node.connect( this.offline_analyser );
+
+        this.offline_processor.onaudioprocess = function(e){
+        	self.offline_overlayImg( e )
+        }
+
+		this.offline_node.buffer = this.buffer; 
+		this.offline_node.start();
+        this.octx.startRendering();
 	},
 
 	setNode: function(){
-		this.node = this.actx.createBufferSource();
-		this.node.buffer = this.buffer;
-		this.node.connect( this.actx.destination );
+		this.node 			=	this.actx.createBufferSource();
+		this.node.buffer 	=	this.buffer;
+		this.node.connect( 		this.actx.destination );
+	},
+
+	setClrs: function(){
+		// garish
+		var clrs = [
+			[ 255, 0, 0 ],
+			[ 255, 127, 0 ],
+			[ 255, 255, 0 ],
+			[ 0, 255, 0 ],
+			[ 0, 0, 255 ],
+			[ 75, 0, 130 ],
+			[ 143, 0, 255 ]
+		];
+
+		var range = parseInt( 127/(clrs.length-1) );
+
+		var clr = 0;
+		for (var i=0; i < 127; i += range){
+			for (var j=0; j <= range; j++){
+				this.freq_rgb[i+j] = clrs[clr];
+			}
+			clr ++;
+		}		
 	}
 });
 
+
 /* Convert to PcmImages all DOM elements selected by 
    the 'selector' supplied as the sole argument, which
-   defaults to '.pcmimg' */
+   defaults to '.pcmimg' 
+   Could have read <audio> nodes and used their
+   media element node, but want Ajax loading at times.
+*/
 PcmImage.parseDOM = function( selector ){
 	selector = selector || '.pcmimg';
 	$$( selector ).each( function(el){
 		var opts = {
-			element:		 el,
-			uri:			 el.dataset.uri,
+			element: el,
+			uri:	 el.dataset.uri,
 		};
 		Object.keys(el.dataset).each( function(i){
 			opts[i] = el.dataset[i];
